39f0b6270f38f07310131bcc3f822742c7574d2d
ningjc909 <ningjc@szlanyou.com>
2023-04-16 15:36:23 +0800

    【修改描述】: 商品页增加解析商品信息的功能，以及对商品的图片和文字进行了布局 【修改类型】: 新增功能 【修改时间】：2023-04-16 【修改人员】：Sandro

--- pages/shoplist/shoplist.js
+++ pages/shoplist/shoplist.js
@@ -6,16 +6,38 @@ Page({
    */
   data: {
     //保存传递过来的页面参数
-    query:[]
+    query:[],
+    shopList:[],
+    page:1,
+    pageSize:10,
+    total:0
   },
-
+  getShopInfo(){
+    wx.request({
+      url: 'https://www.escook.cn/categoires/${this.data.query.id}/shops',
+      method:'get',
+      data:{
+        _page:this.data.page,
+        _limit:this.data.pageSize
+      },
+      success:(res)=>{
+        this.setData({
+          shopList:[...this.data.shopList,...res.data],
+          //字符转数字
+          total:res.header["X-Total-Count"] - 0
+        }),
+        console.log(this.data.shopList)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
       this.setData({
         query:options
-      })
+      }),
+      this.getShopInfo()
   },
 
   /**
