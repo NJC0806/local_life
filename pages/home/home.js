b0feb7ecb855b7caf034b9ac96b6ddddfcd10134
ningjc909 <ningjc@szlanyou.com>
2023-04-16 12:56:41 +0800

    【修改描述】: 实现轮播图的效果 【修改类型】: 新增功能 【修改时间】：2023-04-16 【修改人员】：Sandro

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,21 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //存放轮播图数据
+    swipperList:[]
   },
-
+  //获取轮播图的相关图片地址
+  getSwipperList(){
+    wx.request({
+      url: 'https://www.escook.cn/slides',
+      method:'GET',
+      success:(res) => {
+          this.setData({
+            //对应的轮播图数据在res.data中
+            swipperList: res.data
+          })
+          console.log(this.data.swipperList)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    //页面加载时就因该发起请求
+    this.getSwipperList()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
   },
 
   /**
